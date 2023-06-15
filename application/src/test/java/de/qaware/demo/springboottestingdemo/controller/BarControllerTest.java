0fb8bcebde4419ff1307e0919eb304adcb5ab875
Andreas Grub <andreas.grub@qaware.de>
2023-06-15 08:51:31 +0200

    feat: use @WebMvcTest slice

--- application/src/test/java/de/qaware/demo/springboottestingdemo/controller/BarControllerTest.java
+++ application/src/test/java/de/qaware/demo/springboottestingdemo/controller/BarControllerTest.java
@@ -1,11 +1,9 @@
 package de.qaware.demo.springboottestingdemo.controller;
 
 import de.qaware.demo.springboottestingdemo.repository.BarRepository;
-import de.qaware.demo.springboottestingdemo.repository.FooRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,13 +14,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = BarController.class)
 @TestPropertySource(properties = "bar.defaultResponse = bar bar bar yeah")
 class BarControllerTest {
-
-    @MockBean
-    private FooRepository fooRepository;
 
     @MockBean
     private BarRepository barRepository;
--- application/src/test/java/de/qaware/demo/springboottestingdemo/controller/FooControllerTest.java
+++ application/src/test/java/de/qaware/demo/springboottestingdemo/controller/FooControllerTest.java
@@ -1,11 +1,9 @@
 package de.qaware.demo.springboottestingdemo.controller;
 
-import de.qaware.demo.springboottestingdemo.repository.BarRepository;
 import de.qaware.demo.springboottestingdemo.repository.FooRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,15 +11,11 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = FooController.class)
 class FooControllerTest {
 
     @MockBean
     private FooRepository fooRepository;
-
-    @MockBean
-    private BarRepository barRepository;
 
     @Autowired
     private MockMvc mvc;
