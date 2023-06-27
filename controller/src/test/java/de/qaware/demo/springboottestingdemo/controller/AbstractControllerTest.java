bcc7eaa59e7dd3dc9bc0f431b0561fa2878a7bf8
Andreas Grub <andreas.grub@qaware.de>
2023-06-27 13:22:26 +0200

    feat: move controller test to controller module

--- controller/src/test/java/de/qaware/demo/springboottestingdemo/controller/AbstractControllerTest.java
+++ controller/src/test/java/de/qaware/demo/springboottestingdemo/controller/AbstractControllerTest.java
@@ -3,12 +3,14 @@ package de.qaware.demo.springboottestingdemo.controller;
 import de.qaware.demo.springboottestingdemo.repository.BarRepository;
 import de.qaware.demo.springboottestingdemo.repository.FooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 abstract class AbstractControllerTest {
     @Autowired
